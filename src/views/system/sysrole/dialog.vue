501ed214da9fff998d165d9afc168d68d17e075e
lzb <1394465750@qq.com>
2023-07-08 21:40:00 +0800

    角色管理界面

---
+++ src/views/system/sysrole/dialog.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="system-dic-dialog-container">
+    <el-dialog :title="state.dialog.title" v-model="state.dialog.isShowDialog" width="769px">
+      <el-form ref="ruleFormRef" :model="state.dataForm" :rules="rules" size="default" label-width="90px">
+        <el-row :gutter="35">
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+            <el-form-item label="角色名称" prop="roleName">
+              <el-input v-model="state.dataForm.roleName" placeholder="请输入角色名称" clearable></el-input>
+            </el-form-item>
+          </el-col>
+
+
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+            <el-form-item label="角色状态">
+              <el-switch v-model="state.dataForm.status" inline-prompt active-text="启" inactive-text="禁"></el-switch>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb20">
+            <el-form-item label="角色描述">
+              <el-input v-model="state.dataForm.remark" type="textarea" placeholder="请输入角色描述"
+                        maxlength="100"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+            <el-form-item label="排序">
+              <el-input-number v-model="state.dataForm.roleSort" :min="0" :max="999" controls-position="right"
+                               placeholder="请输入排序" class="w100"/>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb20">
+            <el-form-item label="菜单权限">
+              <el-tree ref="treeRef"  node-key="id" :data="state.menuData"
+                       :default-checked-keys="state.dataForm.roleMenuIds"  :props="state.menuProps" show-checkbox class="menu-data-tree"/>
+            </el-form-item>
+          </el-col>
+
+        </el-row>
+      </el-form>
+      <template #footer>
+				<span class="dialog-footer">
+					<el-button @click="onCancel" size="default">取 消</el-button>
+					<el-button type="primary" @click="onSubmit(ruleFormRef)" size="default">{{
+              state.dialog.submitTxt
+            }}</el-button>
+				</span>
+      </template>
+    </el-dialog>
+
+
+  </div>
+</template>
+
+<script setup lang="ts" name="systemDicDialog">
+import {reactive, ref} from 'vue';
+import {sysroleApi} from '/@/api/sysrole/index';
+import {ElMessage} from "element-plus";
+import type {FormInstance, FormRules} from 'element-plus'
+// 定义子组件向父组件传值/事件
+const emit = defineEmits(['refresh']);
+import {useMenuApi} from '/@/api/menu/index';
+// 定义变量内容
+const ruleFormRef = ref();
+const treeRef = ref<InstanceType<typeof ElTree>>()
+const rules = reactive<FormRules>({
+
+  roleName: [
+    {
+      required: true,
+      message: '角色名称不能为空!',
+      trigger: 'blur',
+    }
+
+  ]
+  ,
+  roleSort: [
+    {
+      required: true,
+      message: '显示顺序不能为空!',
+      trigger: 'blur',
+    }
+
+  ]
+  ,
+  status: [
+    {
+      required: true,
+      message: '角色状态（0正常 1停用）不能为空!',
+      trigger: 'blur',
+    }
+
+  ]
+  ,
+  delFlag: [
+    {
+      required: true,
+      message: '删除标志（0代表存在 1代表删除）不能为空!',
+      trigger: 'blur',
+    }
+
+  ]
+  ,
+  createBy: [
+    {
+      required: true,
+      message: '创建者不能为空!',
+      trigger: 'blur',
+    }
+
+  ]
+  ,
+  updateBy: [
+    {
+      required: true,
+      message: '更新者不能为空!',
+      trigger: 'blur',
+    }
+
+  ]
+  ,
+
+});
+const state = reactive({
+  menuData: [] as TreeType[],
+  menuProps: {
+    children: 'children',
+    label: 'menuName',
+  },
+  dataForm: {
+    roleMenuIds:[],
+    roleName: '' // 角色名称
+    ,
+    roleSort: '0' // 显示顺序
+    ,
+    status: '' // 角色状态（0正常 1停用）
+    ,
+    delFlag: '' // 删除标志（0代表存在 1代表删除）
+    ,
+    createBy: '' // 创建者
+    ,
+    updateBy: '' // 更新者
+    ,
+  },
+  dialog: {
+    isShowDialog: false,
+    type: '',
+    title: '',
+    submitTxt: '',
+  },
+  type: "",
+});
+const getMenuData = async () => {
+  const menuApi = await useMenuApi();
+  await menuApi.getMenu().then(value => {
+    state.menuData = value.data.children;
+  });
+}
+// 打开弹窗
+const openDialog = async (type: string, row) => {
+  state.type = type;
+  //加载菜单
+  await getMenuData();
+  if (type === 'edit') {
+    const dic = await sysroleApi();
+    await dic.info({id: row.id}).then(value => {
+      let data = value.data;
+      state.dataForm = data;
+    });
+    state.dialog.title = '修改字典';
+    state.dialog.submitTxt = '修 改';
+  } else {
+    state.dialog.title = '新增字典';
+    state.dialog.submitTxt = '新 增';
+    // 清空表单，此项需加表单验证才能使用
+    // nextTick(() => {
+    // 	dicDialogFormRef.value.resetFields();
+    // });
+  }
+  state.dialog.isShowDialog = true;
+};
+// 关闭弹窗
+const closeDialog = () => {
+  state.dialog.isShowDialog = false;
+};
+// 取消
+const onCancel = () => {
+  closeDialog();
+};
+const close = () => {
+  state.dataForm = {
+    roleName: '' // 角色名称
+    ,
+    roleSort: '0' // 显示顺序
+    ,
+    status: '' // 角色状态（0正常 1停用）
+    ,
+    delFlag: '' // 删除标志（0代表存在 1代表删除）
+    ,
+    createBy: '' // 创建者
+    ,
+    updateBy: '' // 更新者
+    ,
+  };
+  closeDialog();
+  emit('refresh');
+}
+// 提交
+const onSubmit = async (formEl: FormInstance | undefined) => {
+  const dic = await sysroleApi();
+  if (!formEl) return
+  state.dataForm.roleMenuIds=treeRef.value.getCheckedKeys();
+  formEl.validate((valid, fields) => {
+    if (valid) {
+      if (state.type == 'add') {
+       // console.log(treeRef.value.getCheckedKeys().join())
+          dic.save(state.dataForm).then(value => {
+            if (value.code == '200') {
+              ElMessage.success(value.message);
+            } else {
+              ElMessage.error(value.message);
+            }
+            close();
+          })
+      } else {
+        //修改
+        dic.update(state.dataForm).then(value => {
+          if (value.code == '200') {
+            ElMessage.success(value.message);
+          } else {
+            ElMessage.error(value.message);
+          }
+          close();
+        })
+      }
+    }
+  })
+
+};
+
+// 暴露变量
+defineExpose({
+  openDialog,
+});
+</script>
---
+++ src/views/system/sysrole/index.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="system-dic-container layout-padding">
+    <el-card shadow="hover" class="layout-padding-auto">
+      <div class="system-user-search mb15">
+        <el-input size="default" placeholder="请输入查询内容" style="max-width: 180px"></el-input>
+        <el-button size="default" type="primary" class="ml10" @click="getTableData">
+          <el-icon>
+            <ele-Search/>
+          </el-icon>
+          查询
+        </el-button>
+        <el-button size="default" type="success" class="ml10" @click="onOpenAddDic('add')">
+          <el-icon>
+            <ele-FolderAdd/>
+          </el-icon>
+          新增
+        </el-button>
+      </div>
+      <el-table :data="state.tableData.data" v-loading="state.tableData.loading" style="width: 100%">
+        <el-table-column type="index" label="序号" width="60"/>
+        <el-table-column prop="id" label="角色ID" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="roleName" label="角色名称" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="roleSort" label="显示顺序" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="status" label="角色状态" show-overflow-tooltip>
+          <template #default="scope">
+            <dict-tag dictType="sys_role_status" :value="scope.row.status"/>
+          </template>
+        </el-table-column>
+
+        <el-table-column prop="remark" label="备注" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="createBy" label="创建者" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="updateBy" label="更新者" show-overflow-tooltip></el-table-column>
+        <el-table-column label="操作" width="150" >
+          <template #default="scope" >
+            <div v-if="scope.row.id !==1" >
+              <el-button size="small" :icon="Edit" text type="primary" @click="onOpenEditDic('edit', scope.row)">修改
+              </el-button>
+              <el-button size="small" text :icon="Delete" type="danger" @click="onRowDel(scope.row)">删除</el-button>
+            </div>
+
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+          @size-change="onHandleSizeChange"
+          @current-change="onHandleCurrentChange"
+          class="mt15"
+          :pager-count="5"
+          :page-sizes="[10, 20, 30]"
+          v-model:current-page="state.tableData.param.currentPage"
+          background
+          v-model:page-size="state.tableData.param.pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="state.tableData.total"
+      >
+      </el-pagination>
+    </el-card>
+    <DicDialog ref="dicDialogRef" @refresh="getTableData()"/>
+  </div>
+</template>
+
+<script setup lang="ts" name="systemDic">
+import {
+  Delete,
+  Edit,
+} from '@element-plus/icons-vue'
+import {defineAsyncComponent, reactive, onMounted, ref} from 'vue';
+import {ElMessageBox, ElMessage} from 'element-plus';
+import {sysroleApi} from '/@/api/sysrole/index';
+import {useMenuApi} from "/@/api/menu";
+// 引入组件
+const DicDialog = defineAsyncComponent(() => import('/@/views/system/sysrole/dialog.vue'));
+const DictTag = defineAsyncComponent(() => import('/@/components/DictTag/index.vue'));
+// 定义变量内容
+const dicDialogRef = ref();
+const state = reactive({
+  tableData: {
+    data: [],
+    total: 0,
+    loading: false,
+    param: {
+      currentPage: 1,
+      pageSize: 5,
+    },
+  },
+});
+
+// 初始化表格数据
+const getTableData = async () => {
+  state.tableData.loading = true;
+  const dic = await sysroleApi();
+  await dic.queryPage(state.tableData.param).then(data => {
+    // debugger
+    const dataList = data.data.records;
+    state.tableData.data = dataList;
+    state.tableData.total = data.data.total;
+    state.tableData.loading = false;
+  })
+
+};
+
+// 打开新增弹窗
+const onOpenAddDic = (type: string) => {
+  dicDialogRef.value.openDialog(type);
+};
+// 打开修改弹窗
+const onOpenEditDic = (type: string, row) => {
+  dicDialogRef.value.openDialog(type, row);
+};
+// 删除字典
+const onRowDel = (row) => {
+  ElMessageBox.confirm(`此操作将永久删:“${row.id}”，是否继续?`, '提示', {
+    confirmButtonText: '确认',
+    cancelButtonText: '取消',
+    type: 'warning',
+  })
+      .then(async () => {
+        const dic = await sysroleApi();
+        let json = {
+          id: row.id
+        }
+        await dic.delete(json).then(data => {
+          if (data.code == '200') {
+            ElMessage.success(data.message);
+          } else {
+            ElMessage.error(data.message);
+          }
+          getTableData();
+        })
+
+      })
+      .catch(() => {
+      });
+};
+// 分页改变
+const onHandleSizeChange = (val: number) => {
+  state.tableData.param.pageSize = val;
+  getTableData();
+};
+// 分页改变
+const onHandleCurrentChange = (val: number) => {
+  state.tableData.param.pageNum = val;
+  getTableData();
+};
+// 页面加载时
+onMounted(() => {
+  getTableData();
+});
+</script>
